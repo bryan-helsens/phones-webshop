a385062a39170e948c143e96bb00ddd3d71af221
bryan.helsens <bryan.helsens@student.howest.be>
2021-05-18 17:34:57 +0200

    route login

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -15,22 +15,18 @@ class AuthController extends Controller
 
     public function login(Request $request){
 
-        dd($request->all());
-
         $validator = Validator::make($request->all(), [
             'email' => 'required|email',
             'password' => 'required|string|min:6',
         ]);
 
-        if ($validator->fails()){
+        /*if ($validator->fails()){
             return response()->json($validator->errors(), 422);
-        }
+        }¨*/
 
         if (!$token = auth()->attempt($validator->validated())){
-            return response()->json(["error" => "Unauthorized"], 401);
+            return response()->json(["message" => "Unauthorized"], 401);
         }
-
-        dd("Hello");
 
         return response([
             'message' => 'Success',
